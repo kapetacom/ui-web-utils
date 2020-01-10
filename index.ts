8ebf4daf4020bbcd34efe391f039cd1c413c5946
Henrik von Hofmeister <hh@cloudscrape.com>
2020-01-10 11:21:11 +0100

    Added missing dependencies

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
-export * from './src/Environment'
-export * from './src/FileUtils'
-export * from './src/httpUtils'
-export * from './src/reactUtils'
-export * from './src/svg/BasisCurve'
-export * from './src/svg/createConnectionPath'
-export * from './src/svg/createHexagonPath'
-export * from './src/svg/SVGCornersHelper'
-export * from './src/css/classNameUtils'
+export * from './src/Environment';
+
+export * from './src/FileUtils';
+
+export * from './src/httpUtils';
+
+export * from './src/reactUtils';
+
+export * from './src/svg/BasisCurve';
+
+export * from './src/svg/createConnectionPath';
+
+export * from './src/svg/createHexagonPath';
+
+export * from './src/svg/SVGCornersHelper';
+
+export * from './src/css/classNameUtils';
