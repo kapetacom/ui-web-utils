ebb2eda482f0e9d77dfe435459955714b3dc49af
athinodoros <athinodoros.sgouromallis@hotmail.com>
2020-02-24 14:34:49 +0100

    DEXI-912 : make all shape vectors nnumbers (remove.toFixed)

--- src/svg/SVGCornersHelper.ts
+++ src/svg/SVGCornersHelper.ts
@@ -152,29 +152,29 @@ var createRoundedPathString = (pathCoords: { x: number, y: number }[], radius: n
 
         // Calculate curvePoint c1 -> c2
         const c1c2Distance = Math.sqrt(Math.pow(c1.x - c2.x, 2) + Math.pow(c1.y - c2.y, 2));
-        const c1c2DistanceRatio = (c1c2Distance - curveRadius) / c1c2Distance;
+        const c1c2DistanceRatio =(c1c2Distance - curveRadius) / ((c1c2Distance===0)?1:c1c2Distance);
         const c1c2CurvePoint = [
-            ((1 - c1c2DistanceRatio) * c1.x + c1c2DistanceRatio * c2.x).toFixed(1),
-            ((1 - c1c2DistanceRatio) * c1.y + c1c2DistanceRatio * c2.y).toFixed(1)
+            (1 - c1c2DistanceRatio) * c1.x + c1c2DistanceRatio * c2.x,
+            (1 - c1c2DistanceRatio) * c1.y + c1c2DistanceRatio * c2.y
         ];
-
+       
         // Calculate curvePoint c2 -> c3
-        const c2c3Distance = Math.sqrt(Math.pow(c2.x - c3.x, 2) + Math.pow(c2.y - c3.y, 2));
-        const c2c3DistanceRatio = curveRadius / c2c3Distance;
+        const c2c3Distance = +Math.sqrt(Math.pow(c2.x - c3.x, 2) + Math.pow(c2.y - c3.y, 2));
+        const c2c3DistanceRatio = curveRadius / ((c2c3Distance===0)?1:c2c3Distance);
         const c2c3CurvePoint = [
-            ((1 - c2c3DistanceRatio) * c2.x + c2c3DistanceRatio * c3.x).toFixed(1),
-            ((1 - c2c3DistanceRatio) * c2.y + c2c3DistanceRatio * c3.y).toFixed(1)
+            +((1 - c2c3DistanceRatio) * c2.x + c2c3DistanceRatio * c3.x),
+            +((1 - c2c3DistanceRatio) * c2.y + c2c3DistanceRatio * c3.y)
         ];
-
+        
         // If at last coord of polygon, also save that as starting point
         if (i === pathCoords.length - 1) {
             path.unshift('M' + c2c3CurvePoint.join(','));
         }
-
+        
         // Line to start of curve (L endcoord)
         path.push('L' + c1c2CurvePoint.join(','));
         // Bezier line around curve (Q controlcoord endcoord)
-        path.push('Q' + c2.x + ',' + c2.y + ',' + c2c3CurvePoint.join(','));
+        path.push('Q' + Math.round(c2.x) + ',' + Math.round(c2.y) + ',' + c2c3CurvePoint.join(','));
     }
     // Logically connect path to starting point again (shouldn't be necessary as path ends there anyway, but seems cleaner)
     path.push('Z');
@@ -257,4 +257,4 @@ let getResourceTagPath = (height: number, width: number, radius: number, side?: 
     }
 };
 
-export const SVGCornersHelper = { roundPathCorners, createRoundedPathString, getResourceTagPath,getResourceTag3_25 };
+export const SVGCornersHelper = { roundPathCorners, createRoundedPathString, getResourceTagPath, getResourceTag3_25 };
